b95437b1c8ecc5c4e25c85c047a5be47165f4989
Keemply <144055348+Keemply@users.noreply.github.com>
2024-10-01 23:06:20 +0300

    finish 2.0

--- src/main.js
+++ src/main.js
@@ -6,21 +6,27 @@ import { apiRequest } from './js/pixabay-api';
 import { render } from './js/render-functions';
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery-list');
+const loader = document.querySelector('.loader');
+const simpleGallery = new simpleLightbox('.simple-gal', {
+  captionsData: 'alt',
+});
+function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+function hideLoader() {
+  loader.classList.add('is-hidden');
+}
 function submitHandler(event) {
   event.preventDefault();
-  gallery.innerHTML = '<span class="loader"></span>';
-
+  gallery.innerHTML = '';
+  showLoader();
   apiRequest(form[0].value)
     .then(data => {
-      gallery.innerHTML = '';
+      hideLoader();
       const dataJson = data.hits;
       gallery.insertAdjacentHTML('beforeend', render(dataJson));
-      const simpleGallery = new simpleLightbox('.simple-gal', {
-        captionsData: 'alt',
-      });
-      return simpleGallery;
     })
-    .then(simpleGallery => {
+    .then(() => {
       simpleGallery.refresh();
     })
     .catch(error => {
